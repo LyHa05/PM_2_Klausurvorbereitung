2c7604a009f6bfd2e7a830c37cd3740e964b66df
Lydia <lydia.pflug@web.de>
2016-06-24 08:56:22 +0200

    Auto-Klasse erstellt

---
+++ src/lydia/a02/Auto.java
@@ -0,0 +1,69 @@
+package lydia.a02;
+
+/** 
+ * @author Lydia Pflug
+ * @datum 24.06.2016
+ * Klasse, die ein Auto beschreibt.
+ * */
+
+public class Auto {
+
+	public String farbe;
+	
+	public int ps;
+	
+	private int anzahlReifen;
+	
+	/**Konstruktor*/
+	public Auto(String farbe, int ps, int aReifen){
+		this.farbe = farbe;
+		this.ps = ps;
+		this.anzahlReifen = aReifen;
+	}
+	
+	/**Copy-Konstruktor*/
+	public Auto(Auto autoObjekt) {
+		this.farbe = autoObjekt.getFarbe();
+		this.ps = autoObjekt.getPS();
+		this.anzahlReifen = autoObjekt.getAnzahlReifen();
+	}
+
+	/**Getter-Methode*/
+	private String getFarbe() {
+		return farbe;
+	}
+
+	/**Setter-Methode*/
+	private void setFarbe(String farbe) {
+		this.farbe = farbe;
+	}
+
+	/**Getter-Methode*/
+	private int getPS() {
+		return ps;
+	}
+
+	/**Setter-Methode*/
+	private void setPS(int ps) {
+		this.ps = ps;
+	}
+
+	/**Getter-Methode*/
+	private int getAnzahlReifen() {
+		return anzahlReifen;
+	}
+
+	/**Setter-Methode*/
+	private void setAnzahlReifen(int anzahlReifen) {
+		this.anzahlReifen = anzahlReifen;
+	}
+
+	/**Ueberschriebene toString Methode*/
+	@Override
+	public String toString() {
+		return "Ein Auto in " + farbe + " mit " + ps + " PS und " + anzahlReifen + "Reifen.";
+	}
+	
+	
+	
+}
--- src/lydia/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author Lydia
- *
- */
-package lydia;
